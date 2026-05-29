cc4456163b9dbe2816f6f9fffe09fa2524ac35a6
Artur Bagautdinov <arturbagautdinov30@yahoo.com>
2026-05-29 19:04:36 +0300

    Initial Commit

---
+++ VibeFinder/VibeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeFinderApp.swift
+//  VibeFinder
+//
+//  Created by Artur Bagautdinov on 29.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct VibeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeFinderTests/VibeFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeFinderTests.swift
+//  VibeFinderTests
+//
+//  Created by Artur Bagautdinov on 29.05.2026.
+//
+
+import Testing
+@testable import VibeFinder
+
+struct VibeFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
